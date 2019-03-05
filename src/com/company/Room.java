f4830509fe2f058a22a370a2fd7c14b5dfa2e963
Shae Coud <shadycloud88@yahoo.com>
2019-03-05 09:08:59 -0700

    Update Room.java
    This commit includes the addition of Java doc comments.

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -3,23 +3,51 @@ package com.company;
 
 // This is my room "factory"
 public class Room {
+    /**
+     * Whether this room is visitable.
+     */
     boolean isVisitable;
+    /**
+     * The x coordinate of this room
+     */
     int x;
+    /**
+     * The y coordinate of this room
+     */
     int y;
+    /**
+     * The variable to keep track of traveled path
+     */
     boolean isPartOfPath;
-    char character;
+    /**
+     * The character variables used to represent rooms and walls
+     */
+    char ch;
 
-    public Room(int x, int y, char ch) { // Constructor to create an instance of the 'Room' class
+    /**
+     * Room constructor to create an instance of the 'Room' class
+     *
+     * @param x  location coordinate for each maze room
+     * @param y  location coordinate for each maze room
+     * @param ch variable to indicate weather a room is visitable
+     */
+    public Room(int x, int y, char ch) {
         this.y = y;
         this.x = x;
-        this.isVisitable = ch == ' '; // This defines a room being visitable and sets it equal to 'ch' below
-        this.isPartOfPath = false; // This defines a room being part of successful path
-        this.character = ch;
+        // Set this room to be visitable if it is a blank space
+        if (ch == ' ') {
+            this.isVisitable = true;
+        } else { // If the cell is a wall, not visitable is set
+            this.isVisitable = false;
+        }
+        // Default: unvisited rooms have yet to become part of path
+        this.isPartOfPath = false;
+        this.ch = ch;
     }
-    // Prints successful path and shows how each room is set to not visitable after the explore method is called
-    public String toString() {
-        return "(" + x + ", " + y + " " + isVisitable + ")";
 
+    @Override
+    public String toString() {
+        return "" + this.ch;
     }
 }
 
